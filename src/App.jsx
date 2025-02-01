eedd1de8db12efe30af6de0daa8c3c5a9fd19f2d
Rajesh144142 <rajeshkh704435@gmail.com>
2025-02-02 01:37:16 +0530

    added new chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/hero_section';
-import Navbar from './components/navigation_bar';
-import Education from './components/education-section';
+// import Navbar from './components/navigation_bar';
+// import Education from './components/education-section';
 import './App.css';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Hero />} />
-                {/* <Route path="/education" element={<Education />} /> */}
-            </Routes>
-            {/* <Navbar /> */}
-        </Router>
+        // <Router>
+        //     <Routes>
+        //         <Route path="/" element={<Hero />} />
+        //         {/* <Route path="/education" element={<Education />} /> */}
+        //     </Routes>
+        //     {/* <Navbar /> */}
+        // </Router>
+        <>
+            <Hero/>
+        </>
     );
 }
 
